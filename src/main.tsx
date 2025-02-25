6a0868b4f0959ef1f41f90d88964f7367d8f94fc
Otávio Daniel <dotavioaug@gmail.com>
2025-02-25 18:15:51 -0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import reactDOM from 'react-dom/client'
+import App from './App.tsx'
+
+reactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
